84e6bf424d988ea105fa06df40b4fbe94d57eae8
gabrieldamiaoreal <gbussnisses@outlook.com>
2021-08-06 20:47:02 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exemplo_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo C#!");
+        }
+    }
+}
